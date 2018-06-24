ed490fa74078fd826551c9968d5dc5709ef7a332
Ivan Lavrov <ilavrov@yandex-team.ru>
2018-06-24 16:18:09 +0300

    fragment

---
+++ app/src/main/java/ru/yandex/ilavrov/constraintlayoutdemo/ImageFragment.kt
@@ -0,0 +1,15 @@
+package ru.yandex.ilavrov.constraintlayoutdemo
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ImageFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_image, container, false)
+    }
+}
