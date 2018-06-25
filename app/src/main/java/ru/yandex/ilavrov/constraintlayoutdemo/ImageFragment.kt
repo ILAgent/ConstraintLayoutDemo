c609f12965ca43109b23c5b8a37f4c348652db88
Ivan Lavrov <ilavrov@yandex-team.ru>
2018-06-25 11:58:04 +0300

    add image and listener

--- app/src/main/java/ru/yandex/ilavrov/constraintlayoutdemo/ImageFragment.kt
+++ app/src/main/java/ru/yandex/ilavrov/constraintlayoutdemo/ImageFragment.kt
@@ -5,11 +5,17 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class ImageFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_image, container, false)
+                .also {
+                    it.findViewById<TextView>(R.id.expand).setOnClickListener {
+
+                    }
+                }
     }
 }
